7daf1b2f62ef0e76d947d0227930f615b085e969
rrrobo <rrrobo@icloud.com>
2018-10-24 23:39:32 +0900

    update setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-sudo docker run -d --name rcj-scoring-japan -p 3000:80  -v "$(pwd)"/db:/data/db -v "$(pwd)"/logs:/opt/rcj-scoring-system/logs -v "$(pwd)"/TechnicalDocument:/opt/rcj-scoring-system/TechnicalDocument --restart=always ryorobo/rcj-rescue-scoring-japan
+echo "必要なディレクトリを作成します"
+mkdir -p logs
+mkdir -p db
+mkdir -p TechnicalDocument
+echo "最新版のイメージをDocker Hubよりダウンロードします"
+sudo docker pull ryorobo/rcj-rescue-scoring-japan:latest
+echo "イメージを起動します"
+sudo docker run -d --name rcj-scoring-japan -p 3000:80  -v "$(pwd)"/db:/data/db -v "$(pwd)"/logs:/opt/rcj-scoring-system/logs -v "$(pwd)"/TechnicalDocument:/opt/rcj-scoring-system/TechnicalDocument --restart=always ryorobo/rcj-rescue-scoring-japan:latest
